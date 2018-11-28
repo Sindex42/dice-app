0d15437d35c2daadb55bc71841d566952b3f1e70
Connor Kam-Cheong <connorkc42@gmail.com>
2018-11-28 22:48:01 +0000

    Added roll-multiple

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,4 +1,10 @@
 class Dice
   def roll
+    rand(1..6)
   end
+
+  def roll_multiple(num)
+    self.roll + self.roll
+  end
+
 end
--- spec/dice._spec.rb
+++ spec/dice._spec.rb
@@ -1,7 +1,17 @@
 require 'dice'
 
 describe Dice do
-  it { is_expected.to respond_to(:roll) }
+  let(:die) {Dice.new}
 
-  
+  it { is_expected.to respond_to(:roll) }
+  it { is_expected.to respond_to(:roll_multiple) }
+
+  it 'returns a random number between 1 and 6' do
+    expect(die.roll).to be_between(1, 6).inclusive
+  end
+
+  it 'returns a number between 2 and 12' do
+    expect(die.roll_multiple(1)).to be_between(2, 12).inclusive
+  end
+
 end
